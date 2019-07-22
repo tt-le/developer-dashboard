6f8e388cd8561247420c4f5185d369b2cf65178c
Sean Sundberg <seansund@us.ibm.com>
2019-07-21 23:38:56 -0500

    Update server/index.js to get port from process.env.PORT

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
-async function configureServer(port) {
+async function configureServer() {
   const app = new express();
 
   app.use(express.static('build'));
 
-  app.listen(port || 8080);
-  console.log(`Server listening on port: ${port || 8080}`);
+  const port = process.env.PORT || 8080;
+
+  app.listen(port);
+  console.log(`Server listening on port: ${port}`);
 }
 
 configureServer().catch(err => console.log('Error running server', err));
