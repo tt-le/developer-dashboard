7399b35070c2987ccb0b192f7c77daeda699e985
Matthew Perrins <mjperrin@us.ibm.com>
2020-01-10 20:08:24 -0600

    Update server to run in local mode

--- server/routers/urls.js
+++ server/routers/urls.js
@@ -2,43 +2,51 @@ var express = require('express');
 const _ = require('lodash');
 const kubernetesClient = require('./kubernetes-client');
 
-module.exports = function(app) {
-  var router = express.Router();
+module.exports = function (app) {
+    var router = express.Router();
 
-  router.get('/', function (req, res, next) {
+    router.get('/', function (req, res, next) {
 
-    try {
+            function respondUrls(res,env) {
+                return res.json({
+                    gitlab: "https://" + (env.REGION || 'us-south') + ".git.cloud.ibm.com",
+                    che: env.CHE_URL || "https://che.openshift.io/dashboard/",
+                    jenkins: env.JENKINS_URL,
+                    pipeline: env.PIPELINE_URL,
+                    argocd: env.ARGOCD_URL,
+                    artifactory: env.ARTIFACTORY_URL,
+                    sonarqube: env.SONARQUBE_URL,
+                    pact: env.PACTBROKER_URL,
+                    tekton: env.TEKTON_URL,
+                    ta: env.TA_URL,
+                    swagger: env.SWAGGER_URL
+                });
+            }
 
-      const client = kubernetesClient();
+            if (process.env.npm_lifecycle_event === "dev") {
+                respondUrls(res,process.env);
+            } else {
+                try {
 
-      const qs = {labelSelector: 'group=catalyst-tools'};
-      client.api.v1.namespace(process.env.NAMESPACE || 'tools').configmaps.get({qs})
-          .then(result => {
-            return _.assign({}, process.env, ...(_.get(result, 'body.items', []).map(configmap => configmap.data)));
-          }, error => {
-            console.error('Error reading config maps: ', error);
-            return process.env;
-          })
-          .then(env => {
-            res.json({
-              gitlab: "https://" + (env.REGION || 'us-south') + ".git.cloud.ibm.com",
-              che: env.CHE_URL || "https://che.openshift.io/dashboard/",
-              jenkins: env.JENKINS_URL,
-              pipeline: env.PIPELINE_URL,
-              argocd: env.ARGOCD_URL,
-              artifactory: env.ARTIFACTORY_URL,
-              sonarqube: env.SONARQUBE_URL,
-              pact: env.PACTBROKER_URL,
-              tekton: env.TEKTON_URL,
-              ta: env.TA_URL,
-              swagger: env.SWAGGER_URL
-            });
-          });
-    } catch (err) {
-      console.error('Error configuring kubeClient: ', err);
-      res.json({});
-    }
-  });
+                    const client = kubernetesClient();
+                    const qs = {labelSelector: 'group=catalyst-tools'};
+                    client.api.v1.namespace(process.env.NAMESPACE || 'tools').configmaps.get({qs})
+                        .then(result => {
+                            return _.assign({}, process.env, ...(_.get(result, 'body.items', []).map(configmap => configmap.data)));
+                        }, error => {
+                            console.error('Error reading config maps: ', error);
+                            return process.env;
+                        })
+                        .then(env => {
+                            respondUrls(res,env);
+                        });
+                } catch (err) {
+                    console.error('Error configuring kubeClient: ', err);
+                    res.json({});
+                }
+            }
+        }
+    );
 
-  app.use("/urls", router);
+    app.use("/urls", router);
 };
